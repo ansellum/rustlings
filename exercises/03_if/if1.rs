400c35a60efed4e5df0bae8b2f99be8d4416ff3c
marche <jacobvogt27@gmail.com>
2026-03-08 15:40:48 -0700

    Updated exercises

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,22 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+
+    if a < b {
+        b
+    } else {
+        a
+    }
+
 }
 
 fn main() {
     // You can optionally experiment here.
+
+    let num1 = 6;
+    let num2 = 4;
+
+    println!("The bigger number between {0} and {1} is {2}", num1, num2, bigger(num1, num2));
 }
 
 // Don't mind this for now :)
